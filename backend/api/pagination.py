9307575596754769252dadef834213f6d215d448
Dmitry Korepanov' aka DNK <dnker@mail.ru>
2023-05-23 02:36:06 +0500

    PEP8

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -2,7 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 from django.conf import settings
 
-
 class LimitPageNumberPagination(PageNumberPagination):
     """ Пагинация. """
     page_size = settings.DEFAULT_PAGE_SIZE
