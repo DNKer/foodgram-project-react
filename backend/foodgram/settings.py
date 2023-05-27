9188d20c50370ac1c8e97477b0168f71fdad7a0d
Dmitry Korepanov' aka DNK <dnker@mail.ru>
2023-05-27 12:48:43 +0500

    settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -12,8 +12,7 @@ load_dotenv()
 
 DEFAULT_PAGE_SIZE: int = 6
 
-BASE_DIR = os.path.dirname(
-    os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 SECRET_KEY = os.getenv(
     'SECRET_KEY',
@@ -22,7 +21,10 @@ SECRET_KEY = os.getenv(
 
 DEBUG = os.getenv('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv(
+    'ALLOWED_HOSTS',
+    default=['localhost']
+).split(', ')
 
 AUTH_USER_MODEL = 'users.User'
 
