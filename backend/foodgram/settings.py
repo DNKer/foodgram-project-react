340e1a7ce2f273e41bba5fb32d206d7b30dbfccd
Dmitry Korepanov' aka DNK <dnker@mail.ru>
2023-05-27 08:41:05 +0500

    workflow

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.getenv(
 DEBUG = os.getenv('DEBUG', default=False)
 
 ALLOWED_HOSTS = os.environ.get(
-    'ALLOWED_HOSTS', default='localhost').split(', ')
+    'ALLOWED_HOSTS', default='*').split(', ')
 
 AUTH_USER_MODEL = 'users.User'
 
