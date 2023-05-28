61247b405e7e56ff87b71bd61c9cd9ae9dae4665
Dmitry Korepanov' aka DNK <dnker@mail.ru>
2023-05-28 16:44:57 +0500

    serializers

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -48,10 +48,10 @@ class AuthSerializer(serializers.ModelSerializer):
                 raise serializers.ValidationError(
                     'Не удается войти в систему с '
                     'указанными учетными данными.')
-            if not email or password:
-                raise serializers.ValidationError(
-                    'Необходимо указать "адрес электронной '
-                    'почты" и "пароль".')
+        elif not email or password:
+            raise serializers.ValidationError(
+                'Необходимо указать "адрес электронной '
+                'почты" и "пароль".')
         data['user'] = user
         return data
 
