b7b65401b8bbb064120ad8ec8ee34bd17e9bab2d
Dmitry Korepanov' aka DNK <dnker@mail.ru>
2023-05-29 18:37:06 +0500

    settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -12,8 +12,7 @@ load_dotenv()
 
 DEFAULT_PAGE_SIZE: int = 6
 
-BASE_DIR = os.path.dirname(
-    os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 SECRET_KEY = os.getenv(
     'SECRET_KEY',
