4d4195609ab69f706fb63207ac117f5ef4170176
Dmitry Korepanov' aka DNK <dnker@mail.ru>
2023-06-03 11:09:54 +0500

    urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,7 +2,6 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import (
-    AuthToken,
     IngredientsViewSet,
     RecipesViewSet,
     set_password,
