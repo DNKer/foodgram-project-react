d51fe413ed619ac99cfd14bdb3cafeb83ce1b0c1
Dmitry Korepanov' aka DNK <dnker@mail.ru>
2023-06-10 10:08:59 +0500

    views

--- backend/api/views.py
+++ backend/api/views.py
@@ -75,7 +75,7 @@ class UserViewSet(DjoserUserViewSet):
                 context={'request': request})
             return Response(serializer.data,
                             status=status.HTTP_201_CREATED)
-        elif request.method == 'DELETE':
+        if request.method == 'DELETE':
             if Subscribe.objects.filter(user=request.user,
                                         author=author
                                         ).exists():
