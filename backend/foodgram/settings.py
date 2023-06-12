df9db394327cf890d054eb6240f3676d806f1808
Dmitry Korepanov' aka DNK <dnker@mail.ru>
2023-06-12 17:06:41 +0500

    settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -98,7 +98,7 @@ else:
             'PASSWORD': os.getenv('POSTGRES_PASSWORD',
                                   default='postgres'),
             'HOST': os.getenv('DB_HOST',
-                              default='db'),
+                              default='foodgram'),
             'PORT': os.getenv('DB_PORT',
                               default='5432')
         }
